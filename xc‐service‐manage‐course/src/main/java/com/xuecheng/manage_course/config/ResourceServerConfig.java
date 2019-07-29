70f5f84694468e2e9774005fe4da548b83325f93
chenlong <17610343381@163.com>
2019-07-29 17:49:18 +0800

    网关功能测试OK

--- xc‐service‐manage‐course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
+++ xc‐service‐manage‐course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
@@ -58,10 +58,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         //所有请求必须认证通过
         http.authorizeRequests()
-        //下边的路径放行
+                //下边的路径放行
                 .antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui",
                         "/swagger-resources","/swagger-resources/configuration/security",
-                        "/swagger-ui.html").permitAll()
+                        "/swagger-ui.html","/course/coursebase/**").permitAll()
                 .anyRequest().authenticated();
     }
 }
